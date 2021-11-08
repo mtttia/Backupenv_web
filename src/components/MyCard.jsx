6b92c9d87f6852d0d54dacee5a80c3ce6b5f3ddf
mtttia <tia369852@gmail.com>
2021-11-08 16:56:49 +0100

    build for github pages

--- src/components/MyCard.jsx
+++
@@ -1,9 +0,0 @@
-import './MyCard.scss'
-
-export default function MyCard(props) {
-  return <div className='mc-back'>
-    <h1 className='mc-title'>{ props.title }</h1>
-    <hr className='mc-hr' />
-    <p className="mc-text">{props.text}</p>
-  </div>
-}
--- src/components/text/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-import './Subtitle.scss';
-
-export default function Subtitle(props){
-    return (
-        <div>
-            <p className="subtitle">{props.text}</p>
-        </div>
-    )
-}
